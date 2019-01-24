a2c40338db6f7583a445156f59f9522fefe46c70
Varun Rustomji <varun0248@gmail.com>
2019-01-24 12:27:44 -0800

    Initial Commit

---
+++ 3DEarth/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  3DEarth
+//
+//  Created by Varun Rustomji on 2019-01-24.
+//  Copyright © 2019 Varun Rustomji. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
